6e3f817ad08d8ccac27c3cc1fa1132cff53a48d0
Ethan Stanks <dev.stanks@proton.me>
2024-01-28 14:59:04 -0500

    Added data for cards. Created .gitignore with train/test/valid photos added to ignore list.

--- src/main.py
+++ src/main.py
@@ -1,17 +1,34 @@
 import cv2
+import math
+import numpy as np
+import os
+
+VIDEO_NAME = 'test'
 
 if __name__ == '__main__':
     # default camera
     webcam = cv2.VideoCapture(0)
 
+    #  set width and height
+    frame_width = int(webcam.get(3))
+    frame_height = int(webcam.get(4)) 
+    frame_size = (frame_width, frame_height)
+
+    # init
+    frame_count = 0
+
     while True:
         is_captured, frame = webcam.read()
-        if is_captured:
-            cv2.imshow('Webcam', frame)
+        frame_count += 1
+        if not is_captured:
+            break
 
-            key = cv2.waitKey(1)
-            if key == 27: # esc
-                break
+        cv2.imshow('Webcam', frame)
+
+
+        key = cv2.waitKey(1)
+        if key == 27: # esc
+            break
 
     webcam.release()
     cv2.destroyAllWindows()
